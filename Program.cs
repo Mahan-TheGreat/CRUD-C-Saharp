580e0c111029683631f856da999439cd43438a08
mahan <mahan.timalsena@danphecare.com>
2023-03-12 17:20:42 +0545

    Added ApplicationDBContext  to the program and also added DatabaseInitializer

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CRUD.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseInMemoryDatabase("CRUD");
+});
+
+builder.Services.AddScoped<IApplicationDBContext, ApplicationDBContext>();
+builder.Services.AddScoped<DatabaseInitializer>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+await scope.ServiceProvider.GetRequiredService<DatabaseInitializer>().SeedProductsData();
+
+app.UseCors(builder =>
+{
+    builder.WithOrigins("http://localhost:4200")
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
