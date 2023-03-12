e94aaf1279f4f3960635eac5d31dd23bb2cf546e
mahan <mahan.timalsena@danphecare.com>
2023-03-12 16:26:15 +0545

    Created a basic project with Visual Studio

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿//using CRUD.Entities;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace CRUD.Controllers;
+
+//[Route("api/[controller]")]
+//[ApiController]
+//public class ProductsController : ControllerBase
+//{
+//    [HttpGet]
+//    public IEnumerable<Product> GetProducts()
+//    {
+//        return 0;
+//    }
+//}
---
+++ Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD.Entities;
+
+public class Product
+{
+    public int id { get; set; }
+    public string name { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
+    public decimal price { get; set; }
+    public int quantity { get; set; }
+    public bool isDiscountApplicable { get; set; }
+    public decimal percentDiscount { get; set;}
+    public decimal priceDiscount { get; set; }
+    public bool isActive { get; set; }
+}
