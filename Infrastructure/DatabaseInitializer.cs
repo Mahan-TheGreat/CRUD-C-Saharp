875aeb4ec1239c5a0124e5839046b7b406370c0d
mahan <mahan.timalsena@danphecare.com>
2023-03-12 17:19:35 +0545

    Added Initializer to add data on the start of the application.

---
+++ Infrastructure/DatabaseInitializer.cs
@@ -0,0 +1,54 @@
+﻿using CRUD.Entities;
+
+namespace CRUD.Infrastructure;
+
+public class DatabaseInitializer
+{
+    public readonly IApplicationDBContext _context;
+
+	public DatabaseInitializer(IApplicationDBContext context)
+	{
+		_context = context;
+	}
+
+	public async Task SeedProductsData()
+	{
+        var Products = new List<Product>()
+        {
+            new Product()
+            {
+                name = "Product1",
+                description = "Lorem Ipsum",
+                price = 20,
+                quantity = 2,
+                isDiscountApplicable = false,
+                isActive = true
+
+            },
+            new Product()
+            {
+                name = "Product2",
+                description = "Lorem Ipsum 2",
+                price = 40,
+                quantity = 5,
+                isDiscountApplicable = false,
+                isActive = true
+
+            },
+            new Product()
+            {
+                name = "Product 3",
+                description = "Lorem Ipsum 3",
+                price = 1000,
+                quantity = 1,
+                isDiscountApplicable = true,
+                percentDiscount = 10,
+                priceDiscount = 100,
+                isActive = true
+
+            }
+        };
+        _context.Products.AddRange(Products);
+        await _context.SaveChangesAsync();
+    }
+}
