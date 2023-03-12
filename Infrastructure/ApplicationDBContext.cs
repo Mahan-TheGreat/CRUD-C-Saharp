5683731250b9e9f8e89a60a32d1a1701294806fc
mahan <mahan.timalsena@danphecare.com>
2023-03-12 17:19:12 +0545

    Added Application DB Context to store data which implements the interface IApplicationDBContext.

---
+++ Infrastructure/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Infrastructure;
+
+public class ApplicationDBContext: DbContext, IApplicationDBContext
+{
+	public ApplicationDBContext(DbContextOptions options): base(options)
+	{
+
+	}
+
+	public DbSet<Product> Products => Set<Product>();
+}
---
+++ Infrastructure/IApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Infrastructure;
+
+public interface IApplicationDBContext
+{
+    DbSet<Product> Products { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
