9da466698fcc6ee9642f236188c34db8f394901c
mahan <mahan.timalsena@danphecare.com>
2023-03-12 17:58:17 +0545

    Added Get Post and PUT requests for required CRUD Operations.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,9 +14,87 @@ public class ProductsController : ControllerBase
     {
         _Context = Context;
     }
+
     [HttpGet]
     public async Task<List<Product>> GetProducts()
     {
         return await _Context.Products.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostProduct(Product p)
+    {
+        _Context.Products.Add(p);
+        await _Context.SaveChangesAsync();
+        return Ok(new
+        {
+            status = 200,
+            message = "Product Added SuccessFully"
+        });
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> EditProduct(int id, Product editedProduct)
+    {
+        var Product = await _Context.Products.FirstAsync(x => x.id == id);
+        if(Product == null)
+        {
+            return BadRequest("Invalid id!");
+        }
+        Product.name = editedProduct.name;
+        Product.description = editedProduct.description;
+        Product.price = editedProduct.price;
+        Product.quantity = editedProduct.quantity;
+        Product.isDiscountApplicable= editedProduct.isDiscountApplicable;
+        Product.isActive= editedProduct.isActive;
+        if(editedProduct.percentDiscount != null)
+        {
+            Product.percentDiscount = editedProduct.percentDiscount;
+        }
+
+        _Context.Products.Update(Product);
+        await _Context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            status = 200,
+            message = "Product Edited SuccessFully"
+        });
+    }
+
+    [HttpPut("disableProduct")]
+    public async Task<IActionResult> DisableProduct(int id)
+    {
+        var Product = await _Context.Products.FirstAsync(x => x.id == id);
+        if (Product == null)
+        {
+            return BadRequest("Invalid id!");
+        }
+        Product.isActive = false;
+        await _Context.SaveChangesAsync();
+        return Ok(new
+        {
+            status = 200,
+            message = "Product disabled SuccessFully"
+        });
+
+    }
+
+    [HttpPut("enableProduct")]
+    public async Task<IActionResult> EnableProduct(int id)
+    {
+        var Product = await _Context.Products.FirstAsync(x => x.id == id);
+        if (Product == null)
+        {
+            return BadRequest("Invalid id!");
+        }
+        Product.isActive = true;
+        await _Context.SaveChangesAsync();
+        return Ok(new
+        {
+            status = 200,
+            message = "Product enabled SuccessFully"
+        });
+
     }
 }
