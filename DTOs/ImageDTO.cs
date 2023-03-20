eaa376e86eb821e1c70ab2409202810be04fcc70
mahan <mahan.timalsena@danphecare.com>
2023-03-20 21:24:06 +0545

    Added DTO for Image and Product

---
+++ DTOs/ImageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD.DTOs;
+
+public class ImageDTO
+{
+    public string FileName { get; set; } = string.Empty;
+
+    public string FilePath { get; set; } = string.Empty;
+
+    public string Base64String { get; set; } = string.Empty;
+}
---
+++ DTOs/productDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD.DTOs;
+
+public class ProductDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ImageDTO ImagePath { get; set; } = new ImageDTO();
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; } 
+    public int Quantity { get; set; }
+    public bool IsDiscountApplicable { get; set; }
+    public decimal? PercentDiscount { get; set; }
+    public bool IsActive { get; set; }
+}
